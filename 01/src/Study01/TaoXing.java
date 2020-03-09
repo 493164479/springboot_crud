94e9583c836d436ae6e82176853bcebf6181ec9a
sky2019 <493164479@qq.com>
2020-03-09 20:26:40 +0800

    This is my second practice

---
+++ 01/src/Study01/TaoXing.java
@@ -0,0 +1,12 @@
+package Study01;
+
+public class TaoXing {
+    public static void main(String[] args) {
+        for (int i = 9; i >0 ; i--) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
